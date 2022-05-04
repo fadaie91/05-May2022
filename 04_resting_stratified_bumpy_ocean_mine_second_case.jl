94b177e2b06298f2cf851e8e11e2a97627c58dc2
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-05-04 11:35:32 -0400

    Update 04_resting_stratified_bumpy_ocean_mine_second_case.jl

--- 04_resting_stratified_bumpy_ocean_mine_second_case.jl
+++ 04_resting_stratified_bumpy_ocean_mine_second_case.jl
@@ -25,7 +25,7 @@ seamount_field = Field{Center, Center, Nothing}(underlying_grid)
 set!(seamount_field, seamount)
 fill_halo_regions!(seamount_field)
 
-grid = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data))
+grid = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data, minimum_fractional_partial_Δz=1))
 #grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
 
 model = HydrostaticFreeSurfaceModel(; grid,
@@ -33,9 +33,9 @@ model = HydrostaticFreeSurfaceModel(; grid,
                                     tracers = :b,
                                     buoyancy = BuoyancyTracer())
 
-#N² = 1e+1
-#bᵢ(x, y, z) = N² * z
-bᵢ(x, y, z) = 1 + z
+N² = 1
+bᵢ(x, y, z) = N² * z
+#bᵢ(x, y, z) = 1 + z
 set!(model, b = bᵢ)
 
 # Simulation                             
