d1e26da8d085e67a56243ee081acc1cabb007ee7
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-05-24 16:58:51 -0400

    Update 24_mask_mesh_fullcell.jl

--- 24_mask_mesh_fullcell.jl
+++ 24_mask_mesh_fullcell.jl
@@ -20,25 +20,24 @@ function show_mask(grid)
 end
 
 underlying_grid = RectilinearGrid(arch,
-                                  size=(16 8), halo=(3, 3), 
-                                  y = (-1, 1),
-                                  z = (-1, 0),
-                                  topology=(Flat, Periodic, Bounded))
+                                         size=(16, 8), halo=(3, 3),
+                                         y = (-1, 1),
+                                         z = (-1, 0),
+                                         topology=(Flat, Periodic, Bounded))
 
 # A bump
 h₀ = 0.5 # bump height
 L = 0.25 # bump width
+
 @inline h(y) = h₀ * exp(- y^2 / L^2)
 @inline seamount(x, y) = - 1 + h(y)
 
 seamount_field = Field{Center, Center, Nothing}(underlying_grid)
 set!(seamount_field, seamount)
 fill_halo_regions!(seamount_field)
-
 grid_with_seamount = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
-#grid_with_seamount = ImmersedBoundaryGrid(grid, GridFittedBoundary(seamount))
 
-### Plot masked region: mask.png
 x, y, z, cc = show_mask(grid_with_seamount)
+
 plt = heatmap(y, z, interior(cc)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Full_cell")
 savefig(plt,"mask_fullcell.png")
