f0d6a2d38c649432495dfc2d8364e609037280fb
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-05-25 10:08:29 -0400

    Create 25_difference_full_partial_seamount.jl

---
+++ 25_difference_full_partial_seamount.jl
@@ -0,0 +1,58 @@
+using Oceananigans
+using Oceananigans.BoundaryConditions: fill_halo_regions!
+using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBottom, PartialCellBottom
+using Printf
+using Plots
+using Oceananigans.ImmersedBoundaries: mask_immersed_field!
+using DataFrames
+
+arch = CPU()
+
+function show_mask(grid)
+
+    c = CenterField(grid)
+    c .= 1
+
+    mask_immersed_field!(c)
+
+    x, y, z = nodes(c)
+
+    return x, y, z, c
+end
+
+underlying_grid = RectilinearGrid(arch,
+                                         size=(16, 8), halo=(3, 3),
+                                         y = (-1, 1),
+                                         z = (-1, 0),
+                                         topology=(Flat, Periodic, Bounded))
+
+# A bump
+h₀ = 0.5 # bump height
+L = 0.25 # bump width
+
+@inline h(y) = h₀ * exp(- y^2 / L^2)
+@inline seamount(x, y) = - 1 + h(y)
+
+seamount_field = Field{Center, Center, Nothing}(underlying_grid)
+set!(seamount_field, seamount)
+fill_halo_regions!(seamount_field)
+grid_with_seamount_full = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
+
+x, y, z, ccf = show_mask(grid_with_seamount_full)
+
+interior(ccf)[1,:,:]'
+
+#plt = heatmap(y, z, interior(ccf)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Full_cell")
+#savefig(plt,"mask_fullcell.png")
+
+grid_with_seamount_partial = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data,minimum_fractional_Δz=1))
+
+x, y, z, ccp = show_mask(grid_with_seamount_partial)
+
+interior(ccp)[1,:,:]'
+
+
+#plt = heatmap(y, z, interior(ccp)[1,:,:]', xlabel = "y", ylabel = "z", title = "Masked Region_Partial_cell")
+#savefig(plt,"mask_partialcell.png")
+
+interior(ccf)[1,:,:]'-interior(ccp)[1,:,:]'
