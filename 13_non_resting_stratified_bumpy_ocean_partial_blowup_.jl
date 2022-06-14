60442431bfaf2f1fa4e15e31e0c0f67fc8d1bc12
Reyhane Fadaie <85972119+fadaie91@users.noreply.github.com>
2022-06-14 12:39:30 -0400

    Update 13_non_resting_stratified_bumpy_ocean_partial_blowup_.jl

--- 13_non_resting_stratified_bumpy_ocean_partial_blowup_.jl
+++ 13_non_resting_stratified_bumpy_ocean_partial_blowup_.jl
@@ -1,119 +1,77 @@
-using Oceananigans
-using Oceananigans.BoundaryConditions: fill_halo_regions!
-using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBottom, PartialCellBottom
 using Printf
-using GLMakie
-
+using Oceananigans
+using Oceananigans.ImmersedBoundaries: ImmersedBoundaryGrid, GridFittedBottom, PartialCellBottom
+#using Oceananigans.ImmersedBoundaries: solid_node
+using Oceananigans.BoundaryConditions: fill_halo_regions!
+using Plots
+using Oceananigans.ImmersedBoundaries: mask_immersed_field!
 
 arch = CPU()
-tracer_advection = CenteredSecondOrder()
-momentum_advection = CenteredSecondOrder()
 
-underlying_grid = RectilinearGrid(arch,
-                                  size=(64, 32), halo=(3, 3), 
-                                  y = (-1, 1),
+function show_mask(grid)
+
+    print("grid = ", grid, "\n")
+    c = CenterField(CPU(), grid)
+    c .= 1
+
+    mask_immersed_field!(c)
+
+    x, y, z = nodes(c)
+
+    return x, y, z, c
+end
+
+grid =  RectilinearGrid(arch,
+                                  size=(160, 20), halo=(3, 3), 
+                                  y = (-4, 4),
                                   z = (-1, 0),
                                   topology=(Flat, Periodic, Bounded))
 
-# A bump
-h₀ = 0.5 # bump height
-L = 0.25 # bump width
+# Gaussian seamount
+h₀ = 0.1 # bump height
+L = 1 # bump width
 @inline h(y) = h₀ * exp(- y^2 / L^2)
 @inline seamount(x, y) = - 1 + h(y)
 
-seamount_field = Field{Center, Center, Nothing}(underlying_grid)
-set!(seamount_field, seamount)
-fill_halo_regions!(seamount_field)
+seamount_field = Field{Center, Center, Nothing}(grid)
+#grid_with_seamount = ImmersedBoundaryGrid(grid, GridFittedBoundary(seamount))
+grid_with_seamount = ImmersedBoundaryGrid(grid, GridFittedBottom(seamount_field.data))
+#grid = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data,minimum_fractional_Δz=0.2))
 
-minimum_fractional_Δz = 0.2
-#immersed_boundaries = [
- #                      PartialCellBottom(seamount_field.data;
-  #                                       minimum_fractional_Δz),
-                       #GridFittedBottom(seamount_field.data)
-   #                   ]
 
-b = []
-v = []
+###
 
-function progress(sim)
-    vmax = maximum(abs, sim.model.velocities.v)
-    @info @sprintf("Iter: %d, time: %.2e, max|v|: %.2e",
-                   iteration(sim), time(sim), vmax)
+#Ny = grid_with_seamount.data.Ny
+Ny = grid_with_seamount.Ny
 
-    return nothing
+topography_index = zeros(Int64, Ny)
+for (iyC, yC) in enumerate(grid_with_seamount.yC[1:Ny])
+    print("y = ", yC, " z = ", grid_with_seamount.zC[1], " ")
+
+    #izC = 1
+    #test = true
+    #while test && izC <= grid_with_seamount.Nz
+    #	  print(izC, " ")
+	 # test =  immersed_cell(1, iyC, izC, grid_with_seamount)
+	  #izC += 1
+    #end
+    #print(" ")
+    
+    #print(immersed_cell(1, iyC, 1, grid_with_seamount)," ")
+    #print(immersed_cell(1, iyC, 2, grid_with_seamount),"\n")
+    #print(solid_node(Center(), Center(), Center(), 1,iyC,1, grid_with_seamount)," ")
+    #print(solid_node(Center(), Center(), Center(), 1,iyC,2, grid_with_seamount),"\n")
+    #topography_index[iyC] = izC - 1 
 end
 
-#for ib in immersed_boundaries
-    #grid = ImmersedBoundaryGrid(underlying_grid, ib)
-    grid = ImmersedBoundaryGrid(underlying_grid, PartialCellBottom(seamount_field.data,minimum_fractional_Δz=0.2))
-    #grid = ImmersedBoundaryGrid(underlying_grid, GridFittedBottom(seamount_field.data))
-    @show grid
+print(topography_index, "\n")
 
-    model = HydrostaticFreeSurfaceModel(; grid,
-                                        tracer_advection,
-                                        momentum_advection,
-                                        coriolis = FPlane(f=0.1),
-                                        tracers = :b,
-                                        buoyancy = BuoyancyTracer())
+# suppose we have a tracer and we are calling it Theta
 
-    N² = 1
-    bᵢ(x, y, z) = N² * z
-    vᵢ(x, y, z) = 0.1
-    #set!(model, b = bᵢ)
-    set!(model, b = bᵢ, v=vᵢ)
+Theta_topography_slice = zeros(Ny)
+for (iyC, yC) in enumerate(grid_with_seamount.grid.yC[1:Ny])
+    Theta_topography_slice[iyC] = Theta[1, iyC, topography_index[iyC]]
+end
 
-    simulation = Simulation(model; Δt=1e-3, stop_iteration=5000)
-    simulation.callbacks[:p] = Callback(progress, IterationInterval(100))
-
-
-    run!(simulation)
-
-    push!(b, Array(interior(model.tracers.b, 1, :, :)))
-    push!(v, Array(interior(model.velocities.v, 1, :, :)))
-#end
-b_partial = b[1]
-#b_full    = b[2]
-#Δb = b_full .- b_partial
-
-v_partial = v[1]
-#v_full    = v[2]
-#Δv = v_full .- v_partial
-
-fig = Figure(resolution=(1200, 1800))
-
-partial_cell_title = @sprintf("PartialCellBottom with ϵ = %.1f", minimum_fractional_Δz)
-ax_bp = Axis(fig[1, 2], title=partial_cell_title)
-#ax_bf = Axis(fig[2, 2], title="GridFittedBottom")
-ax_bd = Axis(fig[3, 2], title="Difference (GridFitted - PartialCell)")
-
-# ax_vp = Axis(fig[1, 3])
-# ax_vf = Axis(fig[2, 3])
-# ax_vd = Axis(fig[3, 3])
-
-color = (:black, 0.5)
-linewidth = 3
-levels = 15
-
-hmbp = heatmap!(ax_bp, b_partial)
-contour!(ax_bp, b_partial; levels, color, linewidth)
-Colorbar(fig[1, 1], hmbp, label="Buoyancy", flipaxis=false)
-
-#hmbf = heatmap!(ax_bf, b_full)
-#contour!(ax_bf, b_full; levels, color, linewidth)
-#Colorbar(fig[2, 1], hmbf, label="Buoyancy", flipaxis=false)
-
-#hmbd = heatmap!(ax_bd, Δb)
-#Colorbar(fig[3, 1], hmbd, label="Buoyancy", flipaxis=false)
-
-#=
-hmvp = heatmap!(ax_vp, v_partial)
-#contour!(ax_vp, v_partial, levels=15)
-#Colorbar(fig[1, 1], hmvp)
-hmvf = heatmap!(ax_vf, v_full)
-#contour!(ax_vf, v_full, levels=15)
-#Colorbar(fig[2, 1], hmvf)
-hmvd = heatmap!(ax_vd, Δv)
-#Colorbar(fig[3, 1], hmvd)
-=#
-
-display(fig)
+plt = plot(yC[1:Ny], Theta_topography_slice)
+display(plt)
